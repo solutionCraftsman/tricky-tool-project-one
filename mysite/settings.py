8eaa857116ff1725c5b285482527b42b1e2ae535
Ayodele <ayodele.glow@gmail.com>
2021-05-19 15:40:54 +0100

    19-05-21

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,16 +80,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PW'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int)
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('DB_ENGINE'),
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PW'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT', cast=int)
+#     }
+# }
 
 
 
@@ -135,5 +137,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DATABASES['default'] = dj_database_url.config(config('DATABASE_URL'))
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
+# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 django_heroku.settings(locals())
